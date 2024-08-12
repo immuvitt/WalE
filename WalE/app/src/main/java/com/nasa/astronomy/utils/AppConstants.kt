fa24f3de63603146e6c6de5429ff1bdd91157252
Imran <immuvitt@gmail.com>
2024-08-12 20:43:54 +0530

    added NASA_APOD API key

--- WalE/app/src/main/java/com/nasa/astronomy/utils/AppConstants.kt
+++ WalE/app/src/main/java/com/nasa/astronomy/utils/AppConstants.kt
@@ -3,5 +3,5 @@ package com.nasa.astronomy.utils
 object AppConstants {
 
     const val API_BASE_URL = "https://api.nasa.gov/"
-    const val NASA_APOD_API = ""
+    const val NASA_APOD_API = "QAveWy4T9V5MrQeVEbqJlSZ10GvSLC7Lojm614Sk"
 }
